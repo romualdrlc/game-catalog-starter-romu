4e66c7eab1751af93670f00f9b07c947f180617c
romuald <romuald.lecorroller@gmail.com>
2021-04-27 12:29:33 +0200

    PR2 route callback

--- src/server.ts
+++ src/server.ts
@@ -4,21 +4,20 @@ import { Db } from "mongodb";
 import nunjucks from "nunjucks";
 import session from "express-session";
 import MongoStore from "connect-mongo";
-// import OAuth2Client, {
-//   OAuth2ClientConstructor,
-// } from "@fewlines/connect-client";
+import OAuth2Client, {
+  OAuth2ClientConstructor,
+} from "@fewlines/connect-client";
 
-// const oauthClientConstructorProps: OAuth2ClientConstructor = {
-//   openIDConfigurationURL:
-//     "https://fewlines.connect.prod.fewlines.tech/.well-known/openid-configuration",
-//   clientID: `${process.env.CONNECT_CLIENT_ID}`,
-//   clientSecret: `${process.env.CONNECT_CLIENT_SECRET}`,
-//   redirectURI: "https://localhost:3000/oauth/callback",
-//   audience: "wdb2g3",
-//   scopes: ["openid", "email"],
-// };
-
-// const oauthClient = new OAuth2Client(oauthClientConstructorProps);
+const oauthClientConstructorProps: OAuth2ClientConstructor = {
+  openIDConfigurationURL:
+    "https://fewlines.connect.prod.fewlines.tech/.well-known/openid-configuration",
+  clientID: `${process.env.CONNECT_CLIENT_ID}`,
+  clientSecret: `${process.env.CONNECT_CLIENT_SECRET}`,
+  redirectURI: "https://localhost:3000/oauth/callback",
+  audience: "wdb2g3",
+  scopes: ["openid", "email"],
+};
+const oauthClient = new OAuth2Client(oauthClientConstructorProps);
 
 export function makeApp(db: Db): core.Express {
   const app = express();
@@ -46,28 +45,15 @@ export function makeApp(db: Db): core.Express {
   app.set("view engine", "njk");
 
   app.get("/", (request: Request, response: Response) => {
-    const url = `https://localhost:3000/oauth/authorize?client_id=${process.env.CONNECT_CLIENT_ID}&redirect_uri=https://localhost:3000/oauth/callback&response_type=code&scope=email%20openid`;
+    const url = `https://localhost:3000/oauth/authorize?client_id=${oauthClientConstructorProps.clientID}&redirect_uri=${oauthClientConstructorProps.redirectURI}&response_type=code&scope=${oauthClientConstructorProps.scopes}`;
     response.render("index", {
       connectLoginURL: url,
     });
   });
 
-  // app.get(
-  //   "/oauth/callback",
-  //   sessionParser,
-  //   (request: Request, response: Response) => {
-  //     oauthClient
-  //       .getTokensFromAuthorizationCode(`${request.query.code}`)
-  //       .then((result) => {
-  //         console.log(result);
-  //         oauthClient
-  //           .verifyJWT(result.access_token, "RS256")
-  //           .then((payload) => {
-  //             response.json(payload);
-  //           });
-  //       });
-  //   }
-  // );
+  app.get("/oauth/callback", (request: Request, response: Response) => {
+    response.json(oauthClient);
+  });
 
   return app;
 }
