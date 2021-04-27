c7899f67435509d957abeeccbf51d0ca2c53110f
romuald <romuald.lecorroller@gmail.com>
2021-04-27 12:05:47 +0200

    PR1.1

--- src/server.ts
+++ src/server.ts
@@ -4,39 +4,39 @@ import { Db } from "mongodb";
 import nunjucks from "nunjucks";
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import OAuth2Client, {
-  OAuth2ClientConstructor,
-} from "@fewlines/connect-client";
+// import OAuth2Client, {
+//   OAuth2ClientConstructor,
+// } from "@fewlines/connect-client";
 
-const oauthClientConstructorProps: OAuth2ClientConstructor = {
-  openIDConfigurationURL:
-    "https://fewlines.connect.prod.fewlines.tech/.well-known/openid-configuration",
-  clientID: `${process.env.CONNECT_CLIENT_ID}`,
-  clientSecret: `${process.env.CONNECT_CLIENT_SECRET}`,
-  redirectURI: "https://localhost:3000/oauth/callback",
-  audience: "wdb2g3",
-  scopes: ["openid", "email"],
-};
+// const oauthClientConstructorProps: OAuth2ClientConstructor = {
+//   openIDConfigurationURL:
+//     "https://fewlines.connect.prod.fewlines.tech/.well-known/openid-configuration",
+//   clientID: `${process.env.CONNECT_CLIENT_ID}`,
+//   clientSecret: `${process.env.CONNECT_CLIENT_SECRET}`,
+//   redirectURI: "https://localhost:3000/oauth/callback",
+//   audience: "wdb2g3",
+//   scopes: ["openid", "email"],
+// };
 
-const oauthClient = new OAuth2Client(oauthClientConstructorProps);
+// const oauthClient = new OAuth2Client(oauthClientConstructorProps);
 
 export function makeApp(db: Db): core.Express {
   const app = express();
 
-  const sessionParser = session({
-    secret:
-      "fdiosfoihfwihfiohwipuiiufbfiuhfisheiushfpihsfpihfifhihfpuhdfshfhfpihwepihpsdhiodghfoihpfhsfphsdpifh",
-    name: "sessionId",
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-      client: client,
-    }),
-    cookie: {
-      secure: process.env.NODE_ENV === "production",
-      expires: new Date(Date.now() + 3600000),
-    },
-  });
+  // const sessionParser = session({
+  //   secret:
+  //     "fdiosfoihfwihfiohwipuiiufbfiuhfisheiushfpihsfpihfifhihfpuhdfshfhfpihwepihpsdhiodghfoihpfhsfphsdpifh",
+  //   name: "sessionId",
+  //   resave: false,
+  //   saveUninitialized: true,
+  //   store: MongoStore.create({
+  //     client: client,
+  //   }),
+  //   cookie: {
+  //     secure: process.env.NODE_ENV === "production",
+  //     expires: new Date(Date.now() + 3600000),
+  //   },
+  // });
 
   nunjucks.configure("views", {
     autoescape: true,
@@ -45,29 +45,29 @@ export function makeApp(db: Db): core.Express {
 
   app.set("view engine", "njk");
 
-  app.get("/", sessionParser, (request: Request, response: Response) => {
-    const url = `https://localhost:3000/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=code&scope=${scopes}`;
+  app.get("/", (request: Request, response: Response) => {
+    const url = `https://localhost:3000/oauth/authorize?client_id=${process.env.CONNECT_CLIENT_ID}&redirect_uri=https://localhost:3000/oauth/callback&response_type=code&scope=email%20openid`;
     response.render("index", {
       connectLoginURL: url,
     });
   });
 
-  app.get(
-    "/oauth/callback",
-    sessionParser,
-    (request: Request, response: Response) => {
-      oauthClient
-        .getTokensFromAuthorizationCode(`${request.query.code}`)
-        .then((result) => {
-          console.log(result);
-          oauthClient
-            .verifyJWT(result.access_token, "RS256")
-            .then((payload) => {
-              response.json(payload);
-            });
-        });
-    }
-  );
+  // app.get(
+  //   "/oauth/callback",
+  //   sessionParser,
+  //   (request: Request, response: Response) => {
+  //     oauthClient
+  //       .getTokensFromAuthorizationCode(`${request.query.code}`)
+  //       .then((result) => {
+  //         console.log(result);
+  //         oauthClient
+  //           .verifyJWT(result.access_token, "RS256")
+  //           .then((payload) => {
+  //             response.json(payload);
+  //           });
+  //       });
+  //   }
+  // );
 
   return app;
 }
